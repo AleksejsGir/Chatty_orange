fff86b2b08b26ae679221916e41c5a058374b2d0
Igor Pronin <igp78@mail.ru>
2025-05-03 15:59:43 +0200

    создана главная страница с приветствием

--- Chatty_orange/settings.py
+++ Chatty_orange/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'  # Куда перенаправлять после входа
+LOGOUT_REDIRECT_URL = '/'  # Куда перенаправлять после выхода
--- socialnet/views.py
+++ socialnet/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def home_view(request):
+    context = {
+        'user': request.user,
+        'is_authenticated': request.user.is_authenticated
+    }
+    return render(request, 'home.html', context)
