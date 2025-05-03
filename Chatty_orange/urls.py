f8d7a3cfa8516bc0d223b59589564b41120433f9
AleksejsGir <giruckisaleksejs@gmail.com>
2025-05-03 19:53:43 +0100

    add Project_structure.md to project and to КУФREADME.md

--- Chatty_orange/urls.py
+++ Chatty_orange/urls.py
@@ -1,22 +1,18 @@
-"""
-URL configuration for Chatty_orange project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-# from django.contrib import admin
-from django.urls import path
+# Chatty_orange/urls.py
+from django.contrib import admin
+from django.urls import path, include # Убедитесь, что include импортирован
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    # Подключаем URL-адреса из приложений:
+    path('users/', include('users.urls', namespace='users')), # Добавляем namespace='users'
+    path('posts/', include('posts.urls', namespace='posts')), # Добавляем namespace='posts'
+    path('subscriptions/', include('subscriptions.urls', namespace='subscriptions')), # Добавляем namespace='subscriptions'
+
+    # TODO: Добавить путь для главной страницы ('') позже
+    # path('', views.home_page, name='home'),
 ]
+
+# <!-- TODO: Добавить URL для главной страницы. -->
+# <!-- TODO: Настроить раздачу медиафайлов в режиме DEBUG (если необходимо). -->
+# <!-- TODO: Подключить URL для django-debug-toolbar (если используется). -->
