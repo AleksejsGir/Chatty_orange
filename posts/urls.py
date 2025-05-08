469ca4f8cad4576bf2f9531c0bd243566e217bd3
Igor Pronin <igp78@mail.ru>
2025-05-08 19:54:07 +0200

    Реализована форма и логика комментирования. Созданы шаблоны для отображения и удаления комментариев

--- posts/urls.py
+++ posts/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     path('create/', views.PostCreateView.as_view(), name='post-create'),
     path('<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/<int:pk>/', views.PostDetailWithComments.as_view(), name='post-detail'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
+    path('post/<int:pk>/', views.PostDetailWithComments.as_view(), name='post-detail'),
+    path('post/<int:pk>/comment/', views.PostCommentView.as_view(), name='post-comment'),  # Новый URL
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='comment-delete'),
     # Здесь будут URL-паттерны для постов
     # path('', views.post_list, name='list'), # Пример будущего пути
 ]
