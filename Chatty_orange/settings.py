6c89279bbfcbe0027a8c6de4dd9492bfcd370328
schneider 1981@list.ru <schneider1981@list.ru>
2025-05-10 14:51:13 +0200

    добавил изменение

--- Chatty_orange/settings.py
+++ Chatty_orange/settings.py
@@ -112,7 +112,7 @@ else:
     DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.sqlite3'),
-        'NAME': os.getenv('DB_NAME') or str(BASE_DIR / 'db.sqlite3'),
+        'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER',''),
         'PASSWORD': os.getenv('DB_PASSWORD',''),
         'HOST': os.getenv('DB_HOST', 'localhost'),
