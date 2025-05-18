63294b36b2ddf9b7a2b1ad2cf40a96008abcd04f
Viktor Yerokov <pokervipdonvip@gmail.com>
2025-05-18 16:14:16 +0200

    добавление ссылок политики конфединциальности и условия пользования

--- Chatty_orange/urls.py
+++ Chatty_orange/urls.py
@@ -5,14 +5,11 @@ from django.urls import path, include
 
 from Chatty_orange import settings
 from posts import views
-from users import views as user_views # Импортируем views из приложения users
+from users import views as user_views  # Импортируем views из приложения users
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
-    path('posts/', include('posts.urls')),
     # Главная страница
-    path('', user_views.home_page_view, name='home'), # Добавлен путь для главной страницы
+    path('', user_views.home_page_view, name='home'),  # Добавлен путь для главной страницы
 
     # Админка
     path('admin/', admin.site.urls),
@@ -22,12 +19,15 @@ urlpatterns = [
     path('posts/', include('posts.urls', namespace='posts')),
     path('subscriptions/', include('subscriptions.urls', namespace='subscriptions')),
     path('accounts/', include('allauth.urls')),
+
+    # Просмотр условий использования и политики конфиденциальности
     path('terms/', views.TermsOfUseView.as_view(), name='terms-of-use'),
     path('privacy/', views.PrivacyPolicyView.as_view(), name='privacy-policy'),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 # <!-- TODO: Настроить раздачу медиафайлов в режиме DEBUG (если необходимо). -->
