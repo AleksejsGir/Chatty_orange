e690b68cfcabbd758a4a0121e6596d06ed35bd53
AleksejsGir <giruckisaleksejs@gmail.com>
2025-05-19 09:30:57 +0100

    Обновил файлы до последней версии для сервера

--- entrypoint.prod.sh
+++ entrypoint.prod.sh
@@ -3,20 +3,15 @@
 # Выход при ошибке
 set -e
 
-# Установка необходимых пакетов
-apt-get update && apt-get install -y netcat-openbsd
-
 # Ожидание готовности базы данных
 echo "Waiting for database..."
-while ! nc -z $DB_HOST $DB_PORT; do
-  sleep 0.1
-done
+sleep 10
 echo "Database is ready!"
 
-echo "Entrypoint: Applying database migrations..."
+# Применение миграций
+echo "Applying migrations..."
 python manage.py migrate --noinput
 
-echo "Entrypoint: Starting Gunicorn..."
-# Запускаем Gunicorn
-# Используем переменную окружения GUNICORN_CMD_ARGS
+# Запуск Gunicorn
+echo "Starting Gunicorn..."
 exec gunicorn Chatty_orange.wsgi:application $GUNICORN_CMD_ARGS
