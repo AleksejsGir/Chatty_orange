626e17e82c58c8db69b18887d9268029adccbfad
AleksejsGir <giruckisaleksejs@gmail.com>
2025-06-10 09:55:20 +0100

    CKEditor ДОБАВЬТЕ СТРОКУ для отключения предупреждения         'versionCheck': False

--- Chatty_orange/settings/base.py
+++ Chatty_orange/settings/base.py
@@ -199,8 +199,11 @@ CKEDITOR_CONFIGS = {
         'height': 300,
         'removePlugins': 'stylesheetparser',
         'allowedContent': True,
+        # ✅ ДОБАВЬТЕ ЭТУ СТРОКУ для отключения предупреждения
+        'versionCheck': False,
     },
 }
+
 JAZZMIN_SETTINGS = {
     "site_title": "Chatty orange",  # Заголовок административной панели
     "site_header": "Chatty orange: Admin",  # Заголовок окна браузера
